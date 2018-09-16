843619ad27e52f7e2053849de64f7542a21177c6
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-16 15:45:42 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -13,38 +13,7 @@ def reformat_languages(languages)
     end
   end
 
-      puts new_hash
+      new_hash
 
 
 end
-reformat_languages({
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-})
